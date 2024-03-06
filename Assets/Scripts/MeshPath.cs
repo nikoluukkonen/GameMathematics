b177d0215e70c8b5f0f85edcb9f3505aa590afd0
nikoluukkonen <144091642+nikoluukkonen@users.noreply.github.com>
2024-03-06 10:46:56 +0200

    MeshPath

--- Assets/Scripts/MeshPath.cs
+++ Assets/Scripts/MeshPath.cs
@@ -10,7 +10,7 @@ public class MeshPath : MonoBehaviour
     public bool Closed = false;
 
     public Transform Mover;
-
+    [SerializeField]
     private int curCurve = 0;
     private Vector3 bezier;
     private float T = 0;
@@ -19,6 +19,25 @@ public class MeshPath : MonoBehaviour
     [SerializeField]
     [Range(0f, 1f)]
     float Tsimulation;
+
+    public List<Vector3> Vertices = new();
+    public List<int> Triangles = new();
+    public List<Vector3> Normals = new();
+    public List<Vector2> UVs = new();
+
+    [Range(3, 100)] public int Segments;
+    [Range(0.1f, 10f)] public float Radius1;
+    [Range(0.1f, 10f)] public float Radius2;
+
+    void Start()
+    {
+        _GenerateMesh();
+    }
+
+    void OnValidate()
+    {
+        _GenerateMesh();
+    }
 
     void Update()
     {
@@ -40,19 +59,30 @@ public class MeshPath : MonoBehaviour
     {
         _DrawBezier();
 
-        OrientedPoint point = _GetBezierPoint(Tsimulation, Points[0].GetAnchorPos(), Points[0].GetControlPos(1), Points[1].GetAnchorPos(), Points[1].GetControlPos(0));
+        OrientedPoint point = OrientedPoint.CalculateOrientedPoint(Tsimulation, Points[curCurve].GetAnchorPos(), Points[curCurve].GetControlPos(1), 
+            Points[curCurve + 1].GetAnchorPos(), Points[curCurve + 1].GetControlPos(0));
         Mover.SetPositionAndRotation(point.pos, point.rot);
 
-        Gizmos.DrawSphere(point.LocalToWorld(Vector3.right * 3), 0.15f);
-        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * 4) + Vector3.up), 0.15f);
-        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * 5) + Vector3.up), 0.15f);
-        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * 5) + Vector3.down), 0.15f);
-        
-        Gizmos.DrawSphere(point.LocalToWorld(Vector3.right * -3), 0.15f);
-        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * -4) + Vector3.up), 0.15f);
-        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * -5) + Vector3.up), 0.15f);
-        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * -5) + Vector3.down), 0.15f);
-        
+        Gizmos.DrawSphere(point.LocalToWorld(Vector3.right * 3), 0.1f);
+        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * 4) + Vector3.up), 0.1f);
+        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * 5) + Vector3.up), 0.1f);
+        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * 5) + Vector3.down), 0.1f);
+
+        Gizmos.DrawSphere(point.LocalToWorld(Vector3.right * -3), 0.1f);
+        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * -4) + Vector3.up), 0.1f);
+        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * -5) + Vector3.up), 0.1f);
+        Gizmos.DrawSphere(point.LocalToWorld((Vector3.right * -5) + Vector3.down), 0.1f);
+
+        Gizmos.DrawLine(point.LocalToWorld(Vector3.right * 3), point.LocalToWorld((Vector3.right * 4) + Vector3.up));
+        Gizmos.DrawLine(point.LocalToWorld((Vector3.right * 4) + Vector3.up), point.LocalToWorld((Vector3.right * 5) + Vector3.up));
+        Gizmos.DrawLine(point.LocalToWorld((Vector3.right * 5) + Vector3.up), point.LocalToWorld((Vector3.right * 5) + Vector3.down));
+        Gizmos.DrawLine(point.LocalToWorld((Vector3.right * 5) + Vector3.down), point.LocalToWorld((Vector3.right * -5) + Vector3.down));
+        Gizmos.DrawLine(point.LocalToWorld((Vector3.right * -5) + Vector3.down), point.LocalToWorld((Vector3.right * -5) + Vector3.up));
+        Gizmos.DrawLine(point.LocalToWorld((Vector3.right * -5) + Vector3.up), point.LocalToWorld((Vector3.right * -4) + Vector3.up));
+        Gizmos.DrawLine(point.LocalToWorld((Vector3.right * -4) + Vector3.up), point.LocalToWorld((Vector3.right * -4) + Vector3.up));
+        Gizmos.DrawLine(point.LocalToWorld((Vector3.right * -4) + Vector3.up), point.LocalToWorld(Vector3.right * -3));
+        Gizmos.DrawLine(point.LocalToWorld(Vector3.right * -3), point.LocalToWorld(Vector3.right * 3));
+
     }
 
     private void _DrawBezier()
@@ -70,23 +100,86 @@ public class MeshPath : MonoBehaviour
     private void _CalcBezier(bool last)
     {
         if (!last)
-            bezier = _GetBezierPoint(T, Points[curCurve].GetAnchorPos(), Points[curCurve].GetControlPos(1), Points[curCurve + 1].GetControlPos(0), Points[curCurve + 1].GetAnchorPos()).pos;
+            bezier = OrientedPoint.CalculateOrientedPoint(T, Points[curCurve].GetAnchorPos(), Points[curCurve].GetControlPos(1), 
+                Points[curCurve + 1].GetControlPos(0), Points[curCurve + 1].GetAnchorPos()).pos;
         else
-            bezier = _GetBezierPoint(T, Points[curCurve].GetAnchorPos(), Points[curCurve].GetControlPos(1), Points[0].GetControlPos(0), Points[0].GetAnchorPos()).pos;
+            bezier = OrientedPoint.CalculateOrientedPoint(T, Points[curCurve].GetAnchorPos(), Points[curCurve].GetControlPos(1), 
+                Points[0].GetControlPos(0), Points[0].GetAnchorPos()).pos;
     }
 
-    private OrientedPoint _GetBezierPoint(float t, Vector3 first_a, Vector3 first_c, Vector3 second_a, Vector3 second_c)
+    private void _GenerateMesh()
     {
-        Vector3 pointA = Vector3.Lerp(first_a, first_c, t);
-        Vector3 pointB = Vector3.Lerp(first_c, second_c, t);
-        Vector3 pointC = Vector3.Lerp(second_c, second_a, t);
+        Vertices.Clear();
+        Triangles.Clear();
+        Normals.Clear();
+        UVs.Clear();
 
-        Vector3 X = Vector3.Lerp(pointA, pointB, t);
-        Vector3 Y = Vector3.Lerp(pointB, pointC, t);
+        _Slice();
 
-        return new(Vector3.Lerp(X, Y, t), Quaternion.LookRotation(Y - X));
+        Mesh mesh = new Mesh();
+        mesh.SetVertices(Vertices);
+        mesh.SetTriangles(Triangles, 0);
+        mesh.SetNormals(Normals);
+        mesh.SetUVs(0, UVs);
+
+        GetComponent<MeshFilter>().sharedMesh = mesh;
     }
 
+
+    private void _Slice()
+    {
+        OrientedPoint point = OrientedPoint.CalculateOrientedPoint(Tsimulation, Points[curCurve].GetAnchorPos(), Points[curCurve].GetControlPos(1), 
+            Points[curCurve + 1].GetAnchorPos(), Points[curCurve + 1].GetControlPos(0));
+
+        Vertices.Add(point.LocalToWorld(Vector3.right * 3));
+        Vertices.Add(point.LocalToWorld((Vector3.right * 4) + Vector3.up));
+        Vertices.Add(point.LocalToWorld((Vector3.right * 5) + Vector3.up));
+        Vertices.Add(point.LocalToWorld((Vector3.right * 5) + Vector3.down));
+
+        Vertices.Add(point.LocalToWorld(Vector3.right * -3));
+        Vertices.Add(point.LocalToWorld((Vector3.right * -4) + Vector3.up));
+        Vertices.Add(point.LocalToWorld((Vector3.right * -5) + Vector3.up));
+        Vertices.Add(point.LocalToWorld((Vector3.right * -5) + Vector3.down));
+
+        Normals.Add(point.LocalToWorld(Vector3.forward));
+        Normals.Add(point.LocalToWorld(Vector3.forward));
+        Normals.Add(point.LocalToWorld(Vector3.forward));
+        Normals.Add(point.LocalToWorld(Vector3.forward));
+        Normals.Add(point.LocalToWorld(Vector3.forward));
+        Normals.Add(point.LocalToWorld(Vector3.forward));
+        Normals.Add(point.LocalToWorld(Vector3.forward));
+        Normals.Add(point.LocalToWorld(Vector3.forward));
+
+        Triangles.Add(2);
+        Triangles.Add(1);
+        Triangles.Add(0);
+
+        Triangles.Add(3);
+        Triangles.Add(2);
+        Triangles.Add(0);
+
+        Triangles.Add(7);
+        Triangles.Add(3);
+        Triangles.Add(0);
+
+        Triangles.Add(0);
+        Triangles.Add(4);
+        Triangles.Add(7);
+
+        Triangles.Add(5);
+        Triangles.Add(7);
+        Triangles.Add(4);
+
+        Triangles.Add(5);
+        Triangles.Add(6);
+        Triangles.Add(7);
+
+    }
+
+    private void _Road()
+    {
+
+    }
 }
 
 public struct OrientedPoint
@@ -104,4 +197,13 @@ public struct OrientedPoint
     {
         return pos + rot * _local;
     }
+
+    public static OrientedPoint CalculateOrientedPoint(float t, Vector3 first_a, Vector3 first_c, Vector3 second_a, Vector3 second_c)
+    {
+        Vector3 X = Vector3.Lerp(Vector3.Lerp(first_a, first_c, t), Vector3.Lerp(first_c, second_c, t), t);
+        Vector3 Y = Vector3.Lerp(Vector3.Lerp(first_c, second_c, t), Vector3.Lerp(second_c, second_a, t), t);
+
+        return new(Vector3.Lerp(X, Y, t), Quaternion.LookRotation(Y - X));
+    }
+
 }
